eaf6fc3b4c1e453cd62225b1551ceeafab723efe
Jacob Roeland <jacob@jacroe.com>
2014-11-16 20:33:51 -0500

    Rename checkin to editCheckin

--- editCheckin.php
+++ editCheckin.php
@@ -1,11 +1,11 @@
 <?php
 require "scalene/Scalene.php";
-$_->load->model(array("foursquare", "places", "checkins"));
 
 if ($_POST["checkinID"])
 {
 	$_->checkins->update($_POST["checkinID"], $_POST["meal"], $_POST["comments"], $_POST["rating"], array_key_exists("complete", $_POST));
-	header("Location: checkin.php?id=".$_POST["checkinID"]);
+	$checkin = $_->checkins->get($_POST["checkinID"]);
+	header("Location: checkins.php?id=".$checkin->placesID);
 }
 
 if ($_GET["id"] && $checkin = $_->checkins->get($_GET["id"]))
@@ -16,4 +16,4 @@ else
 	header("Location: index.php");
 
 $_->view->assign("checkin", $checkin);
-$_->view->display("checkin");
+$_->view->display("editcheckin");
